e21c7b2b0aa06b347e83802d586e69306cfe4f03
Alex Tang <lalextang@msn.com>
2010-11-16 14:07:05 +0800

    set code delete flag false

--- CarsMaintenance/Properties/Settings.Designer.cs
+++ CarsMaintenance/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace CarsMaintenance.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool DeleteFlag {
             get {
                 return ((bool)(this["DeleteFlag"]));
