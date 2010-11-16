513eb72d7193712223ba00c3b799cb76b866b9d3
Alex Tang <lalextang@msn.com>
2010-11-16 14:08:41 +0800

    fix bugs about select non category and child category must start with parent category code

---
+++ CarsMaintenance/Common/Validation/ParentCodeValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarsMaintenance.Common.Validation
+{
+    public class ParentCodeValidator : IValidator
+    {
+        #region IValidator Members
+
+        public Control Control { get; set; }
+        public string ParentCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public bool Validate()
+        {
+            TextBox txt = Control as TextBox;
+            return txt.Text.StartsWith(ParentCode);
+        }
+
+        #endregion
+    }
+}
