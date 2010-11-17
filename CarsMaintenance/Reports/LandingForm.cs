f45173847fc7abffccdadfccd543506e7f5af65e
Alex Tang <lalextang@msn.com>
2010-11-17 11:13:13 +0800

    adjust message in landing form

--- CarsMaintenance/Reports/LandingForm.cs
+++ CarsMaintenance/Reports/LandingForm.cs
@@ -28,8 +28,8 @@ namespace CarsMaintenance.Reports
                         select o).Take(3);
 
             this.line1.Text = "1.欢迎使用工属具管理应用系统，欢迎提供宝贵意见！";
-            this.line2.Text = "2.";
-            this.line3.Text = "3.";
+            this.line2.Text = "";
+            this.line3.Text = "";
             
             int i = 1;
             foreach (Article a in query)
