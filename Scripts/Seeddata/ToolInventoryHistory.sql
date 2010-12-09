173605cffb41d5087aa2426fdec2b00e4ba93514
zhangyh <zhangyh@taoware.com>
2010-12-09 11:42:13 +0800

    删除初始化文件

--- Scripts/Seeddata/ToolInventoryHistory.sql
+++
@@ -1,6 +0,0 @@
-SET IDENTITY_INSERT [dbo].[ToolInventoryHistory] ON
-insert into ToolInventoryHistory(ToolInventoryHistoryID, ToolInventoryHistoryDate, Account, ToolID, UnitPrice,Quantity,PurchaseOrderID, SupplyID)
-select ToolID, PurchaseDate, '1000', ToolID, UnitPrice,Quantity, PurchaseOrderID, SupplyID
-from PurchaseOrder
-SET IDENTITY_INSERT [dbo].[ToolInventoryHistory] OFF
-go
