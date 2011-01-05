1e3a07f2d9d0c30a8ac78690fce12251608a4e23
zhangyh <zhangyh@taoware.com>
2011-01-05 17:29:54 +0800

    更新　 １－４行　更新工具CODE
    ６行　　更新　工具TOOL　的长度和　负载
    
    ８－１５　行　更新　工具　TOOL　规格
    
    １７行　更新　组织机构 UNIT CODE　下划线

--- Scripts/Patch/ChangeCode.sql
+++ Scripts/Patch/ChangeCode.sql
@@ -5,9 +5,15 @@ update Tool set code = replace(code, 'G9', '99')
 update Tool set code = replace(code, 'G', '0') 
 
 
+update tool set length=load2,load2=length  
 
 update tool set Dimensions = 
      case when len([diameter])!=0 then '¦Õ' + [diameter] + 'mm'  else '' end
 	+case when len([load2])!=0 then [load2] + 'T' else '' end
 	+case when len([length])!=0 then '¡Á' + [length] + 'm' else '' end
 	+case when len([Description])!=0 then ', ' + [Description] else '' end from Tool
+
+
+
+update Unit set Code =REPLACE(code,'-','_')
+
